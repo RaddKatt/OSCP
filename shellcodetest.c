80645a22aa4773acaf8dd566bc8f26c66f7b4a54
Radd Katt <RaddKatt0@gmail.com>
2018-06-16 11:47:33 -0700

    Add files via upload

---
+++ shellcodetest.c
@@ -0,0 +1,14 @@
+/*
+shellcodetest.c
+Use this templated script to test out shellcode
+Replace the string "bytecode will go here" with your bytecode
+*/
+
+char code[] = "bytecode will go here!";
+int main(int argc, char **argv)
+
+{
+	int (*func)();
+	func = (int (*)()) code;
+	(int)(*func)();
+}
